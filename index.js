1f3973dee201a8eaf5cc1bc284ec6074f48653ae
João Vicente <joao.vicente@campus.ul.pt>
2020-04-03 16:09:32 +0100

    added log file. Url validations

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 'use strict';
 
-const exec = require('child_process').exec;
+const { exec } = require('child_process');
 const vnu = require('vnu-jar');
 const cors = require('cors');
 const compression = require('compression');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const express = require('express');
 const app = express();
+
 app.use(compression());
 app.use(cors());
 app.use(bodyParser.json({limit: '2mb'}));
 app.use(bodyParser.urlencoded({ extended: false, limit: '2mb' }));
+
+function logError(error) {
+  fs.writeFile('./error.log', error, {'flag':'a'}, (err) => {
+    if (err) console.error(err);
+  });
+}
 
 function fixUrl(url) {
   if(url.startsWith('http://') || url.startsWith('https://')) {
@@ -21,15 +29,42 @@ function fixUrl(url) {
   return 'http://' + url; 
 }
 
+function isValidUrl(url) {
+  try {
+    new URL(url);
+    return true;
+  } catch (err) {
+    logError(err);
+    return false;
+  }
+}
+
 app.get('/url/:url', (req, res) => {
-  const url = decodeURIComponent(req.params.url);
-  console.log(`Validating ${url}`);
-  exec(`java -jar ${vnu} --format json ${fixUrl(url)}`, (error, stdout, stderr) => {
-    res.json(stderr);
-  });
+  const url = decodeURIComponent(req.params.url.trim()).trim();
+  if (url) {
+    if (isValidUrl(fixUrl(url))) {
+      try {
+        exec(`java -jar ${vnu} --format json ${fixUrl(url)}`, (error, stdout, stderr) => {
+          if (error) {
+            logError(error);
+          } else {
+            res.json(stderr);
+          }
+        });
+      } catch (err) {
+        logError(err);
+      }
+    } else {
+      res.statusCode(500);
+    }
+  } else {
+    res.statusCode(500);
+  }
 });
 
-const PORT =  5000;
+logError('');
+
+const PORT = 5555;
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
+  console.log(`Server listening for validation on port ${PORT}...`);
 });
