5ab3365591b226ba385c34b5b013a4d10d17235f
João Vicente <joao.vicente@campus.ul.pt>
2021-11-15 20:33:29 +0000

    Dependencies updated. Bugs fixed

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-'use strict';
+"use strict";
 
-const { exec } = require('child_process');
-const vnu = require('vnu-jar');
-const cors = require('cors');
-const compression = require('compression');
-const bodyParser = require('body-parser');
-const fs = require('fs');
+const { exec } = require("child_process");
+const vnu = require("vnu-jar");
+const cors = require("cors");
+const compression = require("compression");
+const fs = require("fs");
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(compression());
 app.use(cors());
-app.use(bodyParser.json({limit: '2mb'}));
-app.use(bodyParser.urlencoded({ extended: false, limit: '2mb' }));
+app.use(express.json({ limit: "2mb" }));
+app.use(express.urlencoded({ extended: false, limit: "2mb" }));
 
 function logError(error) {
-  fs.writeFile('./error.log', JSON.stringify(error), {'flag':'a'}, (err) => {
-    if (err) console.error(err);
-  });
+  try {
+    fs.writeFile(
+      "./error.log",
+      JSON.stringify(error) + "\n",
+      { flag: "a" },
+      (err) => {
+        if (err) console.error(err);
+      }
+    );
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 function fixUrl(url) {
-  if(url.startsWith('http://') || url.startsWith('https://')) {
+  if (url.startsWith("http://") || url.startsWith("https://")) {
     return url;
   }
 
-  return 'http://' + url; 
+  return "http://" + url;
 }
 
 function isValidUrl(url) {
@@ -39,35 +47,38 @@ function isValidUrl(url) {
   }
 }
 
-app.get('/:url', (req, res) => {
+app.get("/:url", (req, res) => {
   const url = decodeURIComponent(req.params.url.trim()).trim();
-  if (url) {
+  if (url && url.trim() !== "favicon.ico") {
     if (isValidUrl(fixUrl(url))) {
-      try { console.log('Validating: ', url);
-        exec(`java -jar ${vnu} --format json ${fixUrl(url)}`, (error, stdout, stderr) => {
-          if (stderr) {
-            res.status(200).json(stderr);
-          } else {
-            res.status(500).json(error);
+      try {
+        console.log("Validating: ", url);
+        exec(
+          `java -jar ${vnu} --stdout --errors-only --also-check-svg --no-stream --format json ${fixUrl(
+            url
+          )}`,
+          (error, stdout) => {
+            if (stdout) {
+              res.status(200).json(stdout);
+            } else if (error) {
+              logError(error);
+              res.status(500).json(error);
+            }
           }
-
-          if (error) {
-            logError(error);
-          }
-        });
+        );
       } catch (err) {
         logError(err);
-	      res.status(500).json(err);
+        res.status(500).json(err);
       }
     } else {
-      res.status(500).json('Invalid url');
+      res.status(500).json("Invalid url");
     }
   } else {
-    res.status(500).json('Invalid url');
+    res.status(500).json("Invalid url");
   }
 });
 
-logError('');
+logError("HTML Validator error file");
 
 const PORT = 5555;
 app.listen(PORT, () => {
